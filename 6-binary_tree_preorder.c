e897c309d317178043a399e64850da1788be9a4b
adi-mart <aurelie.di-martino@holbertonstudents.com>
2025-08-04 15:25:00 +0200

    Implement binary_tree_preorder function for pre-order traversal of a binary tree

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -9,5 +9,13 @@
  */
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
-    /* Implementation will be added here */
+	if (tree == NULL || func == NULL)
+		return;
+
+	else
+	{
+		func(tree->n);
+		binary_tree_preorder(tree->right, func);
+		binary_tree_preorder(tree->left, func);
+	}
 }
