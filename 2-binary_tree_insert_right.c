ab9a40a124b8d34b1146852abf79b7dc1fabbb6b
adi-mart <aurelie.di-martino@holbertonstudents.com>
2025-08-04 14:13:43 +0200

    Fix parent pointer assignment in binary_tree_insert_right function

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -18,6 +18,7 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 		parent->right = new;
 	else
 	{
+		new->parent = parent;
 		new->right = parent->right;
 		parent->right = new;
 		new->right->parent = new;
