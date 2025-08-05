916a7c232dabe20a4fca5039f0ddf36e9f39723e
adi-mart <aurelie.di-martino@holbertonstudents.com>
2025-08-05 10:34:35 +0200

    Fix binary_tree_is_perfect logic to correctly check for perfect trees

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -40,8 +40,9 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	if (tree->left == NULL || tree->right == NULL)
 		return (0);
 
-	if (binary_tree_height(tree->left) == binary_tree_height(tree->right))
-		return (1);
-	else
+	if (binary_tree_height(tree->left) != binary_tree_height(tree->right))
 		return (0);
+
+	return (binary_tree_is_perfect(tree->left)
+		&& binary_tree_is_perfect(tree->right));
 }
